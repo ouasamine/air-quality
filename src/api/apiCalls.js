62a37ddb7a1282a356ccd43f3225c4d97634c4d1
ouasamine <ouas.amine@outlook.com>
2022-11-24 15:13:59 +0100

    Replcar http with https in API calls

--- src/api/apiCalls.js
+++ src/api/apiCalls.js
@@ -2,14 +2,14 @@ const apiKey = '985888d5c6e9bbd16d041ff9e17d1220';
 
 const getCityIndex = async (lat, long) => {
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${long}&appid=${apiKey}`,
+    `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${long}&appid=${apiKey}`,
   ).then((resp) => resp.json());
   return response;
 };
 
 const getCityCoord = async (city) => {
   const response = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`,
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`,
   ).then((resp) => resp.json());
   return response;
 };
